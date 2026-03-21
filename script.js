810f8bfb8350e30c8bc9896c893e0b4b36e6bc78
Shriyashzzz <ghimires1@my.smccd.edu>
2026-03-21 15:57:08 -0700

    removes topbar, adds © on footer. open Readme.md ls

--- script.js
+++ script.js
@@ -163,3 +163,13 @@ function checkIfdotBtn(){
 
 
 
+const toggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+  }
+});
